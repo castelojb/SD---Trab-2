f9572ec999c110382dd1d166da6a3275d8d2708a
samirchaves <samirbraga@live.com>
2021-02-13 15:37:40 -0300

    Corrigindo nome da pasta da parte 3

--- parte_3/proto_comp.sh
+++
@@ -1,7 +0,0 @@
-SRC_DIR=/home/joao/WorkspacePython/SD---Trab-2/parte_3/
-DST_DIR=/home/joao/WorkspacePython/SD---Trab-2/parte_3/
-
-
-#sudo protoc -I=$SRC_DIR --python_out=$DST_DIR $SRC_DIR/Protobuffer/messages.proto
-
-python -m grpc_tools.protoc -I. --python_out=Protobuffer/ --grpc_python_out=. Protobuffer/messages.proto
