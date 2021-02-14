8fe2d95299b700f57584aa9c970345c4f9b863a2
castelojb <joaocb14@gmail.com>
2021-02-14 17:44:12 -0300

    new way to init a equipament

---
+++ parte_3/equipament_server.py
@@ -0,0 +1,44 @@
+from models import Sensor, Actuator
+import grpc
+from concurrent import futures
+from Protobuffer import messages_pb2_grpc
+import time
+import sys
+
+
+ip = '0.0.0.0'
+
+external_ip = '0.0.0.0'
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv[1:]) != 3:
+        raise Exception('the length of the parameters must be 3: Type Name Port')
+
+    # get args
+    type_ = sys.argv[1]
+    name = sys.argv[2]
+    port = int(sys.argv[3])
+
+    if type_ == 'actuator':
+        eq = Actuator(external_ip, port, name)
+    elif type_ == 'sensor':
+        eq = Sensor(external_ip, port, name)
+    else:
+        raise Exception(f'Received: {type_} must be: actuator or sensor')
+
+    # Run a gRPC server with one thread.
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+
+    # Adds the servicer class to the server.
+    messages_pb2_grpc.add_EquipmentServiceServicer_to_server(eq, server)
+
+    server.add_insecure_port(f'{ip}:{port}')
+
+    server.start()
+
+    print(f'API server started. Listening at {ip}:{port}.')
+
+    while True:
+        time.sleep(60)
